988e0ac7b32a096a9e0a5ada9aa9cce035314ee4
emreturgutce <emrebursalee@gmail.com>
2021-05-25 11:41:44 +0300

    static decorator

--- decorator/decorator.ts
+++ decorator/decorator.ts
@@ -22,16 +22,16 @@ class Squaree implements Shape {
   }
 }
 
-class ColoredShape implements Shape {
-  constructor(private shape: Shape, private color: string) {}
+class ColoredShape<T extends Shape> implements Shape {
+  constructor(private shape: T, private color: string) {}
 
   public info(): string {
     return `${this.shape.info()} has the color ${this.color}`;
   }
 }
 
-class TransparentShape implements Shape {
-  constructor(private shape: Shape, private transparency: number) {}
+class TransparentShape<T extends Shape> implements Shape {
+  constructor(private shape: T, private transparency: number) {}
 
   public info(): string {
     return `${this.shape.info()} has ${this.transparency}% transparency`;
@@ -42,10 +42,10 @@ const circle = new Circle(10);
 console.log(circle.info());
 
 const blueSquare = new ColoredShape(new Squaree(20), 'blue');
-console.log(circle.info());
+console.log(blueSquare.info());
 
 const myCircle = new TransparentShape(
   new ColoredShape(new Circle(5), 'green'),
   50,
 );
-console.log(circle.info());
+console.log(myCircle.info());
