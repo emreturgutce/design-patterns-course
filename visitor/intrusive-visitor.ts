5a2f76fa419ead70c0de4a460b46ad1b3a949d29
emreturgutce <emrebursalee@gmail.com>
2021-06-02 23:44:27 +0300

    intrusive visitor

---
+++ visitor/intrusive-visitor.ts
@@ -0,0 +1,31 @@
+abstract class Expression {
+  public abstract print(): string;
+}
+
+class DoubleExpression extends Expression {
+  constructor(private val: number) {
+    super();
+  }
+
+  public print(): string {
+    return String(this.val);
+  }
+}
+
+class AdditionExpression extends Expression {
+  constructor(private left: Expression, private right: Expression) {
+    super();
+  }
+
+  public print(): string {
+    return `(${this.left.print()}+${this.right.print()})`;
+  }
+}
+
+// 1+(2+3)
+const exp = new AdditionExpression(
+  new DoubleExpression(1),
+  new AdditionExpression(new DoubleExpression(2), new DoubleExpression(3)),
+);
+
+console.log(exp.print());
